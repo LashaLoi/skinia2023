b3c2bbf0971d89ccb71cb1942cf7f422e64169f9
Aliaksei Loi <Lashaloi1409@gmail.com>
2023-04-17 23:57:42 +0300

    update main logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,23 @@
-import Image from "next/image";
+// import Image from "next/image";
 
 import { Links } from "../components/Links";
 
 import styles from "../components/RootLayout/index.module.scss";
-import useMediaQuery from "../lib/hooks/useMediaQuery";
+// import useMediaQuery from "../lib/hooks/useMediaQuery";
 
 export default function Home() {
-  const matches = useMediaQuery("(max-width: 1023px)");
+  // const matches = useMediaQuery("(max-width: 1023px)");
 
   return (
     <>
       <div className={styles.center}>
-        <Image
-          alt="hero"
+        <img
+          loading="lazy"
+          alt="logo"
           src="/tb.png"
-          className="main-logo"
-          height={matches ? 240 : 400}
-          width={matches ? 300 : 520}
+          className="main-logo lg:h-[380px] lg:w-[500px] md:h-[300px] md:w-[380px] h-[240px] w-full"
         />
       </div>
-
       <Links />
     </>
   );
