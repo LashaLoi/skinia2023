e4469dece93cbec77850edb65d08cefa567cc03e
Aliaksei Loi <Lashaloi1409@gmail.com>
2023-04-19 15:36:34 +0300

    add register page

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,9 @@ import styles from "../components/RootLayout/index.module.scss";
 export default function Home() {
   return (
     <>
-      <div className={styles.center}>
-        <div className="lg:h-[380px] lg:w-[500px] md:h-[300px] md:w-[380px] h-[200px] w-[250px]">
-          <Image alt="logo" src="/tb.png" className="main-logo" fill />
+      <div className={`mt-[100px] ${styles.center}`}>
+        <div className="lg:h-[380px] lg:w-[500px] md:h-[300px] md:w-[380px] h-[200px] w-[250px] ">
+          <Image alt="logo" src="/tb.png" className="main-logo " fill />
         </div>
       </div>
       <Links />
---
+++ pages/register.tsx
@@ -0,0 +1,123 @@
+import styles from "../components/RootLayout/index.module.scss";
+
+export default function RegisterPage() {
+  return (
+    <>
+      <div className="mt-[100px]">
+        <div className={styles.center} />
+      </div>
+
+      <div className="w-full relative lg:max-w-xl mb-[200px]">
+        <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">
+          Add a new product
+        </h2>
+        <form action="#">
+          <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="name"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Product Name
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Type product name"
+                required
+              />
+            </div>
+            <div className="w-full">
+              <label
+                htmlFor="brand"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Brand
+              </label>
+              <input
+                type="text"
+                name="brand"
+                id="brand"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Product brand"
+                required
+              />
+            </div>
+            <div className="w-full">
+              <label
+                htmlFor="price"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Price
+              </label>
+              <input
+                type="number"
+                name="price"
+                id="price"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="$2999"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="category"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Category
+              </label>
+              <select
+                id="category"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              >
+                <option selected>Select category</option>
+                <option value="TV">TV/Monitors</option>
+                <option value="PC">PC</option>
+                <option value="GA">Gaming/Console</option>
+                <option value="PH">Phones</option>
+              </select>
+            </div>
+            <div>
+              <label
+                htmlFor="item-weight"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Item Weight (kg)
+              </label>
+              <input
+                type="number"
+                name="item-weight"
+                id="item-weight"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="12"
+                required
+              />
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="description"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Description
+              </label>
+              <textarea
+                id="description"
+                rows={8}
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Your description here"
+              ></textarea>
+            </div>
+          </div>
+          <button
+            type="button"
+            className="text-white mt-4 bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+          >
+            Отправить
+          </button>
+        </form>
+      </div>
+    </>
+  );
+}
